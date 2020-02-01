1ac75f77750d733c4d6a58b82183bbe17c40bf85
mariamisa <maryaam1998@hotmail.com>
2020-02-01 15:38:21 +0200

    make add fuction with test relates #3

--- js/test.js
+++ js/test.js
@@ -11,5 +11,15 @@ test("test my file",function(){
 })
 
 
+test("convert var to object",()=>{
+    const actual=makeObj(1,"todoname",false);
+    const expected={id:1,description:"todoname",done:false};
+    expect(actual).toEqual(expected);
+})
 
 
+test("add new todo to todo array",()=>{
+    const actual=addTodo([],{id:1,description:"name",done:false});
+    const expected=[{id:1,description:"name",done:false}];
+    expect(actual).toEqual(expected);
+})
