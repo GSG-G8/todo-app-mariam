e569047cf6ecdb5135c755292ce94c05900d9c34
mariamisa <maryaam1998@hotmail.com>
2020-02-07 11:14:16 +0200

    refactor logicfunction & add another test for add todo function relates #19

--- js/logic.js
+++ js/logic.js
@@ -24,52 +24,39 @@ var todoFunctions = {
   },
 
   addTodo: function(todos, newTodo) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // returns a new array, it should contain todos with the newTodo added to the end.
-    // add an id to the newTodo. You can use the generateId function to create an id.
-    // hint: array.concat
-    const todosNew = todoFunctions.cloneArrayOfObjects(todos);
-    return todosNew.concat({
+    return todoFunctions.cloneArrayOfObjects(todos).concat({
       id: todoFunctions.generateId(),
       description: newTodo,
       done: false
     });
   },
   deleteTodo: function(todos, idToDelete) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // return a new array, this should not contain any todo with an id of idToDelete
-    // hint: array.filter
-    const newTodo = todoFunctions.cloneArrayOfObjects(todos);
-    return newTodo.filter(el => {
+    return todoFunctions.cloneArrayOfObjects(todos).filter(el => {
       if (el.id !== idToDelete) {
         return el;
       }
     });
   },
   markTodo: function(todos, idToMark) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // in the new todo array, all elements will remain unchanged except the one with id: idToMark
-    // this element will have its done value toggled
-    // hint: array.map
-    const doneTodo = todoFunctions.cloneArrayOfObjects(todos);
-    return doneTodo.map(todo => {
+    return todoFunctions.cloneArrayOfObjects(todos).map(todo => {
       if (todo.id === idToMark) {
-        todo.done === true ? (todo.done = false) : (todo.done = true);
+        todo.done ? (todo.done = false) : (todo.done = true);
         return todo;
       } else {
         return todo;
       }
     });
   },
-//   sortTodos: function(todos, sortFunction) {
-//     // stretch goal! Do this last
-//     // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
-//     // sortFunction will have same signature as the sort function in array.sort
-//     // hint: array.slice, array.sort
-//     const sortTodo = todoFunctions.cloneArrayOfObjects(todos);
-//     return sortTodo.slice(0).sortTodo.sort((a, b) => a - b);
-//   }
-// };
+
+  sortTodos: function(todos, sortFunction) {
+    // stretch goal! Do this last
+    // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
+    // sortFunction will have same signature as the sort function in array.sort
+    // hint: array.slice, array.sort
+    // const sortTodo = todoFunctions.cloneArrayOfObjects(todos);
+    // return sortTodo.slice(0).sortTodo.sort((a, b) => b - a);
+  }
+};
 
 // Why is this if statement necessary?
 // The answer has something to do with needing to run code both in the browser and in Node.js
